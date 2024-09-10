cc235dc0bd5d8dba1c4c5c10398480377c1324a0
Permafrozen <m8tt8o@gmail.com>
2024-09-10 11:50:25 +0200

    added new folder, for user settings, to later on write a bash script

---
+++ external/settings/timo-settings.nix
@@ -0,0 +1,25 @@
+{
+  # System Information
+  username = "timo";
+  keyMap = "at";
+
+  # Theme
+  colorScheme = "gruvbox-dark-soft";
+  polarity = "dark";
+  opacity = "0.7";
+
+  # Hyprland
+  resolution = "2560x1600@60";
+  terminal = "kitty";
+  browser = "qutebrowser";
+  fileManager = "dolphin";
+
+  # Hyprpaper (Wallpaper)
+  wallpaperPath = ".wallpapers/nixos.png"; # From ~/
+
+  # Git Configuration
+  gitUser = "GluttonMain";
+  gitMail = "example@gmail.com";
+  defaultBranch = "main";
+
+}
