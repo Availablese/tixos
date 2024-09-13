31109b5feea09ecae9e2201f356e8451d40ffa2b
GluttonMain <frozenkill.hd@gmail.com>
2024-09-13 10:43:26 +0200

    reformation-script-matteo.sh anpassen

--- bash/reformation-script-matteo.sh
+++ bash/reformation-script-matteo.sh
@@ -2,8 +2,13 @@
 
 sed -i -e 's#vars = import ../../../../core/system/variables.nix#user = import ../../../../users/user.nix;\n  vars = import ../../../../users/${user.user}/variables.nix#g' ~/.modular-nixos-configuration/*/*/*/*/*
 sed -i -e 's#vars = import ../../../core/system/variables.nix#user = import ../../../users/user.nix;\n  vars = import ../../../users/${user.user}/variables.nix#g' ~/.modular-nixos-configuration/*/*/*/*/*
+sed -i -e 's#vars = import ../../../../core/system/hardware-configuration.nix#user = import ../../../../users/user.nix;\n  vars = import ../../../../users/${user.user}/hardware-configuration.nix#g' ~/.modular-nixos-configuration/*/*/*/*/*
 
 mkdir ~/.modular-nixos-configuration/users
+mkdir ~/.modular-nixos-configuration/users/matteo
+mkdir ~/.modular-nixos-configuration/users/timo
 
-mv ~/.modular-nixos-configuration/core/system/variables.nix ~/.modular-nixos-configuration/users
-mv ~/.modular-nixos-configuration/core/hardware-configuration.nix ~/.modular-nixos-configuration/users
+mv ~/.modular-nixos-configuration/core/system/variables.nix ~/.modular-nixos-configuration/users/matteo
+mv ~/.modular-nixos-configuration/core/hardware-configuration.nix ~/.modular-nixos-configuration/users/matteo
+
+
